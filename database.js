e0346d4fed23d4b20755b1cdbd3ce331a359e4c9
Pete Cooke <peterjamescooke@gmail.com>
2023-02-12 15:55:20 +0200

    fix to db creation string, minor tweaks to naming of things

--- database.js
+++ database.js
@@ -1,7 +1,10 @@
-var sqlite3 = require('sqlite3').verbose()
+//can I or do I need to keep the verbose
+import sqlite3 from 'sqlite3';
 import md5 from 'md5';
 
-const DBSOURCE = "db.sqlite"
+const DBSOURCE = "treehouse.sqlite"
+
+console.log ('trying to insert stuff');
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
@@ -15,22 +18,26 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             title TEXT NOT NULL, 
             author TEXT NOT NULL, 
             year INTEGER NOT NULL, 
-            publisher TEXT
-            description TEXT,
+            publisher TEXT,
+            description TEXT
             
             )`,
         (err) => {
             if (err) {
                 // Table already created
+                console.log ('table already created');
             }else{
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO books (title, author, year) VALUES (?,?,?)'
-                db.run(insert, ["Meet The Music Notes","Pete Cooke", 2020])
-                db.run(insert, ["The Music Notes Sing ABC","Pete Cooke", 2022])
+                var insert = 'INSERT INTO books (title, author, year) VALUES (?,?,?)';
+                db.run(insert, ["Meet The Music Notes","Pete Cooke", 2020]);
+                db.run(insert, ["The Music Notes Sing ABC","Pete Cooke", 2022]);
+                console.log ('inserted stuff');
             }
         });  
     }
 });
+
+console.log ('did we insert stuff');
 
 
 export default db
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-// 
+// check ;'s where should they be
 
-const express = require('express');
+import express from 'express';
+import db from './database.js';
+
 const app = express();
 const port = 9000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World Nodemon!');
+  res.send("Welcome to the landing page for Sound Tuition's book collection API.");
 })
+
+app.get("/api/books", (req, res, next) => {
+  var sql = "select * from books"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
