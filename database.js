4f8e5280a69867dd9c77fd08f3af492372f186f4
Pete Cooke <peterjamescooke@gmail.com>
2023-02-12 22:07:06 +0200

    get all response formatted, db renamed and variable names tweaked, single id get endpoint added.

--- database.js
+++ database.js
@@ -2,9 +2,7 @@
 import sqlite3 from 'sqlite3';
 import md5 from 'md5';
 
-const DBSOURCE = "treehouse.sqlite"
-
-console.log ('trying to insert stuff');
+const DBSOURCE = "Book Collection.sqlite"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
@@ -13,31 +11,27 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
       throw err
     }else{
         console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE books (
+        db.run(`CREATE TABLE BookInfo (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL, 
             author TEXT NOT NULL, 
             year INTEGER NOT NULL, 
             publisher TEXT,
             description TEXT
-            
-            )`,
+             )`,
         (err) => {
             if (err) {
                 // Table already created
-                console.log ('table already created');
+                console.log ('Initial book info table already created.');
             }else{
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO books (title, author, year) VALUES (?,?,?)';
+                var insert = 'INSERT INTO BookInfo (title, author, year) VALUES (?,?,?)';
                 db.run(insert, ["Meet The Music Notes","Pete Cooke", 2020]);
                 db.run(insert, ["The Music Notes Sing ABC","Pete Cooke", 2022]);
-                console.log ('inserted stuff');
+                console.log ('Initialised by inserting two entrees');
             }
         });  
     }
 });
-
-console.log ('did we insert stuff');
-
 
 export default db
--- server.js
+++ server.js
@@ -7,27 +7,42 @@ const app = express();
 const port = 9000;
 
 app.get('/', (req, res) => {
-  res.send("Welcome to the landing page for Sound Tuition's book collection API.");
-})
+  res.send("Welcome to the landing page for Sound Tuition's book collection API.")
+});
 
 app.get("/api/books", (req, res, next) => {
-  var sql = "select * from books"
-  var params = []
+  const sql = "SELECT * FROM BookInfo";
+  const params = [];
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({"error":err.message});
         return;
       }
-      res.json({
-          "message":"success",
-          "data":rows
-      })
+      const allBooks = JSON.stringify(rows, null, 2);
+      console.log(allBooks);
+      res.status(200).send("<pre>"+ allBooks +"</pre>");
+         // "message":"success",
+         // "data":rows,  
+      });
     });
-});
 
+    app.get("/api/books/:id", (req, res, next) => {
+      var sql = "SELECT * FROM BookInfo WHERE id = ?"
+      var params = [req.params.id]
+      db.get(sql, params, (err, row) => {
+          if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+          }
+          res.json({
+              "message":"success",
+              "data":row
+          })
+        });
+  });
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Express server listening on port ${port}`);
 })
