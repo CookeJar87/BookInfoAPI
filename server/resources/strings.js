dc7eb28249d0f165ea11c0ae5e01258fbc0d5f5b
Pete Cooke <peterjamescooke@gmail.com>
2023-02-19 21:54:47 +0200

    Attempt to fix gitignore

---
+++ server/resources/strings.js
@@ -0,0 +1,41 @@
+export const directory = `<pre>
+Welcome to the landing page for Sound Tuition's book collection API
+
+GETS
+/ = home,
+/books = get all books,
+/books/id = get a book by id,
+
+POSTS
+/books = post a new book.
+
+PATCHES
+/books = patch an entry.
+
+DELETES
+/books/id = delete a book by id,
+
+</pre>`;
+
+export const resourceNotFoundText = `<pre>
+404 resource not found.
+
+Sadly the resource you are trying to reach is unavailable please try one of the following resources:
+
+GETS
+/ = home,
+/books = all books,
+/books/id = get book by id,
+
+POSTS
+/books = post new book.
+
+PATCHES
+
+DELETES
+/books/id = delete book by id,
+
+Have a top day whatever you are doing.
+</pre>`;
+
+
